7d42d94a260d57fe21a72dcba7bd4bd519670576
SantiagoECHM <111538733+SantiagoECHM@users.noreply.github.com>
2022-11-02 18:58:26 -0600

    videos

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,26 +1,14 @@
 package mx.uv.c80640;
 
+import static spark.Spark.*;
 
 /**
  * Hello world!
  *
  */
-import static spark.Spark.*;
-public class App 
-{
+public class App {
     public static void main(String[] args) {
-        system.out.println("Hello world");
-        get("/Alex", (req, res) -> "Hello World");
-        get("/Eduardo", (req, res) -> "Hello World");
-        get("/Brandon", (req, res) -> "Hello World");
-        get("/", (req, res)->"<h1>Bienvenido</h1><img src ='https://www.uv.mx/v2/images/logouv.jpg'>");
-        post("/", (req, res)->{
-            System.out.println(req.queryParams("email")+""+ req.queryPrams("password"));
-            System.out.println(req.body());
-            res.status(200);
-            JasonObject oRespuesta = new JasonObject();
-            oRespuesta.addProperty("msj", "hola");
-            oRespuesta.addProperty("email", );
-        });
+        System.out.println("Hello World!");
+        get("/hello", (req, res) -> "Hello World12313");
     }
 }
