3f051af697b08ef371dd4da585370ec06dabe988
SantiagoECHM <111538733+SantiagoECHM@users.noreply.github.com>
2022-11-09 21:27:32 -0600

    09/11/22

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -1,6 +1,9 @@
 package mx.uv.c80640;
 
 import static spark.Spark.*;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * Hello world!
@@ -10,5 +13,26 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         get("/hello", (req, res) -> "Hello World12313");
+        post("/", (req, res)->{
+            System.out.println(req.queryParams("email")+""+
+            req.queryParams("password"));
+            System.out.println(req.body());    
+            JsonParser parser = new JsonParser();
+            JsonElement arbol=parser.parse(req.body());
+            JsonObject peticionDelCliente = arbol.getAsJsonObject();
+            System.out.println(peticionDelCliente.get("email").getAsString());
+            System.out.println(peticionDelCliente.get("password"));
+            System.out.println(parser);
+
+            res.status(200);
+            JsonObject oRespuesta = new JsonObject();
+            oRespuesta.addProperty("msj", "hola");
+            //oRespuesta.addProperty("msj", req.queryParams("email"));
+            oRespuesta.addProperty("email", peticionDelCliente.get("email").getAsString());
+
+            return oRespuesta;
+        });
     }
 }
+
+//parser: analizador sintáctico gramatica formal
---
+++ practica07/src/main/java/mx/uv/c80640/App.java
@@ -0,0 +1,39 @@
+package mx.uv.c80640;
+import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sound.sampled.Port;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static Gson gson =new Gson();
+    //base de datos en memoria
+    public static Map<String, Usuario> usuarios =new HashMap<>();
+    public static void main( String[] args )
+    {
+    port(80);
+    //Inicialización de datos
+    Usuario u1 = new Usuario("1", "pablo", "1234");
+    Usuario u2 = new Usuario("2", "Ana", "12345");
+    usuarios.put(u1.getId(), u2);
+    usuarios.put(u2.getId(), u1);
+        System.out.println( "Hello World!" );
+        before((req, res)->res.type("application/json"));
+        get("/usuario", (req, res)->gson.toJson(u1));
+        get("/usuario", (req, res)->gson.toJson(u2));
+        post("/", (req, res)->{
+            String datosFormulario = req.body();
+            Usuario u = gson.fromJson(datosFormulario, Usuario.class);//convertirlo a usuario
+            usuarios.put(u.getId(),u);
+            return "Usuario Agregado";
+        });//curl -X POST -d '{id:"3",nombre:"jose",pass:"123"}' http://localhost/usuarios
+    }
+}
